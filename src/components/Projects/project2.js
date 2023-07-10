b82e5c3b5dd8da28fb3e04baea9523e668181007
MadanM1994 <129888795+MadanM1994@users.noreply.github.com>
2023-07-10 14:39:27 -0400

    Update project2.js

--- src/components/Projects/project2.js
+++ src/components/Projects/project2.js
@@ -80,9 +80,9 @@ function Project2() {
               switch between components based on user actions.
             </li>
             <li>
-              Optimized application functionality using Angular's event
-              handling and state management capabilities and utilized event
-              handlers to promptly respond to user interactions.
+              Optimized application functionality using Angular's event handling
+              and state management capabilities and utilized event handlers to
+              promptly respond to user interactions.
             </li>
             <li>
               Configured the module and executed GET, POST, PUT, and DELETE
@@ -150,8 +150,8 @@ function Project2() {
           </ul>
           <p className="final-element">
             <strong>Technologies:</strong> HTML5, CSS3, Angular, JavaScript,
-            Bootstrap, AJAX, jQuery, RESTful services, RxJS, Jasmine, Karma, MVC,
-            Entity Framework, JIRA, Agile (SCRUM) Development.
+            Bootstrap, AJAX, jQuery, RESTful services, RxJS, Jasmine, Karma,
+            MVC, Entity Framework, JIRA, Agile (SCRUM) Development.
           </p>
           <p>
             Project-
@@ -160,14 +160,14 @@ function Project2() {
             </strong>
           </p>
           <p>
-            <strong>Job Duties:</strong>
+            <strong>Job-Duties:</strong>
           </p>
           <ul>
             <li>
-              Designed and developed web applications using HTML5, CSS3,
-              Angular, JavaScript, Bootstrap, and SCSS, with a focus on using
-              the latest JavaScript ES6 features and Angular frameworks to
-              build dynamic web pages.
+              Designed and developed web applications using HTML5, CSS3, React,
+              JavaScript, Bootstrap, and SCSS, with a focus on using the latest
+              JavaScript ES6 features and React frameworks to build dynamic web
+              pages.
             </li>
             <li>
               Skilled in working with third-party web APIs and web services
@@ -175,7 +175,7 @@ function Project2() {
               JSON files to perform tasks.
             </li>
             <li>
-              Experience in implementing NgRx for state management and for
+              Experience in implementing Redux for state management and for
               logging purposes to make the code loosely coupled.
             </li>
             <li>
@@ -184,88 +184,12 @@ function Project2() {
               production environment.
             </li>
             <li>
-              Developed and designed components using Angular's JSX syntax and
-              built custom components with Angular's component lifecycle methods.
+              Developed and designed components using Reacts JSX syntax and
+              built custom components with Reacts component lifecycle methods.
             </li>
             <li>
-              Utilized Angular Material components for creating UI elements and
-              implemented routing with Angular Router
-
-import "./project2.css";
-
-function Project2() {
-  return (
-    <div>
-      <div className="container-project">
-        <div className="project-details">
-          <h2>THE HOME DEPOT</h2>
-          <p>
-            <strong>Duration:</strong> JULY 2020 – FEBRUARY 2022
-          </p>
-          <p>
-            <strong>Role:</strong> Frontend Developer
-          </p>
-          <p>
-            Project-
-            <strong>HOME DEPOT PRICE ENGINE WEBSITE</strong>
-          </p>
-          <p>
-            <strong>Job Duties:</strong>
-          </p>
-          <ul>
-            <li>
-              Designed and developed websites using HTML5, CSS3, Bootstrap,
-              JavaScript, jQuery, and Angular that met accessibility and web
-              browser standards.
-            </li>
-            <li>
-              Created launch-ready, valid HTML/CSS from wireframes/Zeplins/Figma
-              using the Angular framework and SASS features.
-            </li>
-            <li>
-              Implemented business features using the classical Model View
-              Control (MVC) architecture in front-end components of the Home
-              Depot CMS website.
-            </li>
-            <li>
-              Collaborated with the back-end team to display data securely,
-              utilizing Angular, RxJS, Angular Material, custom components, and
-              back-end APIs.
-            </li>
-            <li>Managed JavaScript packages using Node Package Management.</li>
-            <li>Integrated OAUTH and SSO for application security.</li>
-            <li>
-              Ensured thorough testing of all code before releasing to
-              production.
-            </li>
-            <li>
-              Migrated existing monolithic architecture to microservices in AWS
-              cloud.
-            </li>
-            <li>
-              Utilized Spark Streaming with Kafka as a data pipeline system for
-              real-time processing and core jobs.
-            </li>
-            <li>
-              Created JSON requests and validated responses with Postman
-              Interceptor and Swagger UI.
-            </li>
-            <li>
-              Worked with Web services (SOAP and RESTful) for the transmission
-              of large blocks of XML/JSON.
-            </li>
-            <li>
-              Developed a single ISOMORPHIC responsive website using Angular for
-              desktops, tablets, and mobile users.
-            </li>
-            <li>
-              Maintained states in the stores and dispatched actions using NgRx
-              for state management and implemented Drag and Drop functionality
-              using Angular Material CDK.
-            </li>
-            <li>
-              Used Angular Material components for creating UI elements and
-              implemented routing with Angular Router to create a seamless user
+              Utilized React Material UI library to construct UI elements and
+              implemented routing with React Router to create a seamless user
               experience.
             </li>
             <li>
@@ -273,17 +197,17 @@ function Project2() {
               switch between components based on user actions.
             </li>
             <li>
-              Optimized application functionality using Angular's event
-              handling and state management capabilities and utilized event
-              handlers to promptly respond to user interactions.
+              Optimized application functionality using Reacts event handling
+              and state management capabilities and utilized event handlers to
+              promptly respond to user interactions.
             </li>
             <li>
               Configured the module and executed GET, POST, PUT, and DELETE
               requests, adeptly managing errors and exceptions.
             </li>
             <li>
-              Ensured code functionality with thorough unit testing of Angular
-              components and services using Jasmine and Karma, defining
+              Ensured code functionality with thorough unit testing of React
+              components and services using Jest and Enzyme, defining
               comprehensive test suites and test cases, and creating mock
               dependencies to validate expected results.
             </li>
@@ -295,13 +219,13 @@ function Project2() {
               the web page based on user actions and server responses.
             </li>
             <li>
-              Leveraged MVC design pattern to organize Angular components and
-              used Angular as a framework to create Single Page Applications
+              Leveraged MVC design pattern to organize Reacts components and
+              used React as a framework to create Single Page Applications
               (SPA).
             </li>
             <li>
               Utilized Git for version control, JIRA for defect tracking, and
-              Jasmine/Karma for unit testing.
+              Jest/Enzyme for unit testing.
             </li>
             <li>
               Proficient in CSS pixel-level layout, consistency with browsers,
@@ -310,8 +234,8 @@ function Project2() {
             </li>
             <li>
               Skilled in developing web applications and web services using
-              MVC4, Entity Framework, HTML5, CSS3, JavaScript, jQuery, and
-              Angular.
+              MVC4, Entity Framework, HTML5, CSS3, JavaScript, JQuery, and
+              React.
             </li>
             <li>
               Proven ability to interact with other developers and end-users to
@@ -330,125 +254,7 @@ function Project2() {
               effects during the data load, waiting for the Ajax response.
             </li>
             <li>
-              Developed web application prototypes using jQuery and Angular and
-              developed dynamic pages into JSP pages using Java Servlets,
-              additional Beans, UI, and HTML5 components.
-            </li>
-            <li>
-              Worked on Agile (SCRUM) Development Team to deliver updates to the
-              Business team and Project Manager, attending daily standups,
-              retrospectives, sprint planning, and code review weekly, and
-              completing story points on the JIRA tool.
-            </li>
-          </ul>
-          <p className="final-element">
-            <strong>Technologies:</strong> HTML5, CSS3, Angular, JavaScript,
-            Bootstrap, AJAX, jQuery, RESTful services, RxJS, Jasmine, Karma, MVC,
-            Entity Framework, JIRA, Agile (SCRUM) Development.
-          </p>
-          <p>
-            Project-
-            <strong>
-              REVAMP MANAGEMENT SYSTEM INTERFACE FOR PRODUCT MANAGEMENT TEAM
-            </strong>
-          </p>
-          <p>
-            <strong>Job Duties:</strong>
-          </p>
-          <ul>
-            <li>
-              Designed and developed web applications using HTML5, CSS3,
-              Angular, JavaScript, Bootstrap, and SCSS, with a focus on using
-              the latest JavaScript ES6 features and Angular frameworks to
-              build dynamic web pages.
-            </li>
-            <li>
-              Skilled in working with third-party web APIs and web services
-              using AJAX, jQuery, and RESTful services to create both XML and
-              JSON files to perform tasks.
-            </li>
-            <li>
-              Experience in implementing NgRx for state management and for
-              logging purposes to make the code loosely coupled.
-            </li>
-            <li>
-              Collaborated with cross-functional teams of QA/User Acceptance
-              Testing and Backend to ensure issue tracking and bug fixing in the
-              production environment.
-            </li>
-            <li>
-              Developed and designed components using Angular's JSX syntax and
-              built custom components with Angular's component lifecycle methods.
-            </li>
-            <li>
-              Utilized Angular Material components for creating UI elements and
-              implemented routing with Angular Router
-
-to create a seamless user experience.
-            </li>
-            <li>
-              Established routes for each component and tailored the router to
-              switch between components based on user actions.
-            </li>
-            <li>
-              Optimized application functionality using Angular's event
-              handling and state management capabilities and utilized event
-              handlers to promptly respond to user interactions.
-            </li>
-            <li>
-              Configured the module and executed GET, POST, PUT, and DELETE
-              requests, adeptly managing errors and exceptions.
-            </li>
-            <li>
-              Ensured code functionality with thorough unit testing of Angular
-              components and services using Jasmine and Karma, defining
-              comprehensive test suites and test cases, and creating mock
-              dependencies to validate expected results.
-            </li>
-            <li>
-              Demonstrated expertise in designing the user experience interface
-              UI/UX strategy, UI requirements, and converting findings into UI
-              designs, using JavaScript for interface interactions, animations,
-              and validation, and utilizing the DOM to dynamically manipulate
-              the web page based on user actions and server responses.
-            </li>
-            <li>
-              Leveraged MVC design pattern to organize Angular components and
-              used Angular as a framework to create Single Page Applications
-              (SPA).
-            </li>
-            <li>
-              Utilized Git for version control, JIRA for defect tracking, and
-              Jasmine/Karma for unit testing.
-            </li>
-            <li>
-              Proficient in CSS pixel-level layout, consistency with browsers,
-              version, and platform independence, and worked with CSS3 and
-              Bootstrap to create responsive web designs.
-            </li>
-            <li>
-              Skilled in developing web applications and web services using
-              MVC4, Entity Framework, HTML5, CSS3, JavaScript, jQuery, and
-              Angular.
-            </li>
-            <li>
-              Proven ability to interact with other developers and end-users to
-              design and implement new features throughout the framework and
-              prototyped several versions of possible UI approaches to provide
-              choices to the business.
-            </li>
-            <li>
-              Improved the performance and response time of the application by
-              implementing AJAX and jQuery plug-ins, such as Drag and Drop,
-              AutoComplete, AJAX, and JSON.
-            </li>
-            <li>
-              Proficient in using JavaScript for developing UI components like
-              editable drop-down, data-driven menu customizations, and animation
-              effects during the data load, waiting for the Ajax response.
-            </li>
-            <li>
-              Developed web application prototypes using jQuery and Angular and
+              Developed web application prototypes using jQuery and react and
               developed dynamic pages into JSP pages using Java Servlets,
               additional Beans, UI, and HTML5 components.
             </li>
@@ -460,8 +266,8 @@ to create a seamless user experience.
             </li>
           </ul>
           <p>
-            <strong>Technologies:</strong> HTML5, CSS3, Angular, JavaScript,
-            Bootstrap, AJAX, jQuery, RESTful services, RxJS, Jasmine, Karma, MVC,
+            <strong>Technologies:</strong> HTML5, CSS3, React, JavaScript,
+            Bootstrap, AJAX, jQuery, RESTful services, Redux, Jest, Enzyme, MVC,
             Entity Framework, JIRA, Agile (SCRUM) Development.
           </p>
         </div>
